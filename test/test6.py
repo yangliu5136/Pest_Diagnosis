b0415edca2a0a7929c245d10e84d59188d0158ec
yangliu <13552165136@163.com>
2025-05-08 09:13:41 +0800

    提交

--- test/test6.py
+++ test/test6.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = "https://10.0.0.22/pictureRecognition"
+url = "https://10.0.0.22:5001/pictureRecognition"
 
 payload = json.dumps({
    "pictureUrl": "https://nf-file.hbatg.com/nfshop/MANAGER//9fa3020e98294180aa6e7e620fc248ec.jpg"
