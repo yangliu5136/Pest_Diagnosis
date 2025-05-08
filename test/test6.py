1507f4fde471543a3c2430453ae3da5669b6560c
yangliu <13552165136@163.com>
2025-05-08 09:11:29 +0800

    提交

---
+++ test/test6.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+url = "https://10.0.0.22/pictureRecognition"
+
+payload = json.dumps({
+   "pictureUrl": "https://nf-file.hbatg.com/nfshop/MANAGER//9fa3020e98294180aa6e7e620fc248ec.jpg"
+})
+headers = {
+   'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
